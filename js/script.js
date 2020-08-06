13dbc3da31adb59c49948a76bea1b3414e39a96b
Piotr Bielec <bielec.piotr@gmail.com>
2020-08-06 20:26:42 +0200

    add test-js, first functions

--- js/script.js
+++ js/script.js
@@ -1,24 +1,33 @@
+function getMoveName(argMoveId){
+    if(argMoveId == 1){
+      return 'kamień';
+    }
+  
+    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+    return 'nieznany ruch';
+  }
+
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-console.log('Wylosowana liczba to: ' + randomNumber);
+console.log('Wylosowana liczba to: ' + randomNumber);+
 
-let computerMove = 'nieznany ruch';
+let computerMove = getMoveName(randomNumber);
 
-if(randomNumber == 1){
+/* if(randomNumber == 1){
   computerMove = 'kamień';
 } else if (randomNumber == 2){
   computerMove = 'papier';
 } else if (randomNumber == 3){
   computerMove = 'nożyce';
-}
+} */
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
 
-let playerMove = 'nieznany ruch';
+let playerMove = getMoveName(playerInput);
 
-if(playerInput == '1'){
+/* if(playerInput == '1'){
   playerMove = 'kamień';
 } else if(playerInput == '2'){
   playerMove = 'papier';
@@ -27,7 +36,7 @@ if(playerInput == '1'){
 } else { 
     alert('Wprowadź poprawną liczbę!');
     location.reload();
-}
+} */
 
 printMessage('Mój ruch to: ' + computerMove + '. Twój to: ' + playerMove + '.')
 
