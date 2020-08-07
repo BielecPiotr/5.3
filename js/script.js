a08cbb19ccd494e3e95812f6d185a4483a8d8a83
Piotr Bielec <bielec.piotr@gmail.com>
2020-08-07 17:36:41 +0200

    html fixed, buttons ended

--- js/script.js
+++ js/script.js
@@ -57,3 +57,15 @@ let whoWin = displayResult(argComputerMove, argPlayerMove);
 alert(whoWin);
 
 } 
+
+document.getElementById('play-rock').addEventListener('click', function(){
+    playGame(1);
+  });
+
+document.getElementById('play-paper').addEventListener('click', function(){
+    playGame(2);
+  });
+
+document.getElementById('play-scissors').addEventListener('click', function(){
+    playGame(3);
+  });
